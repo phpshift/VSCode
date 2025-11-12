09f7de8a8208219288a4fc1e2b63e96e5566adc8
IG-onGit <gziraklirex@gmail.com>
2025-11-12 22:27:35 +0400

    + upload

--- extension.js
+++ extension.js
@@ -2,21 +2,29 @@ const vscode = require('vscode');
 
 function activate(context) {
     const projectUrl = 'https://phpshift.com/?vscode';
+    const docsUrl = 'https://docs.phpshift.com/?vscode';
 
     vscode.window.showInformationMessage(
         `Check PHPShift's website for the installation guide: ${projectUrl}`,
-        'Open Website'
+        'Open Website',
+        'Open Documentation'
     ).then(selection => {
         if (selection === 'Open Website') {
             vscode.env.openExternal(vscode.Uri.parse(projectUrl));
+        } else if (selection === 'Open Documentation') {
+            vscode.env.openExternal(vscode.Uri.parse(docsUrl));
         }
     });
 
-    let disposable = vscode.commands.registerCommand('phpshift.openWebsite', () => {
+    let openWebsite = vscode.commands.registerCommand('phpshift.openWebsite', () => {
         vscode.env.openExternal(vscode.Uri.parse(projectUrl));
     });
 
-    context.subscriptions.push(disposable);
+    let openDocs = vscode.commands.registerCommand('phpshift.openDocs', () => {
+        vscode.env.openExternal(vscode.Uri.parse(docsUrl));
+    });
+
+    context.subscriptions.push(openWebsite, openDocs);
 }
 
 function deactivate() {}
